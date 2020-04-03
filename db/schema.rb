49519cecc009c429e905e51ba06f03b020c73eea
osamu.tanaka <radwimps174@gmail.com>
2020-04-03 12:46:37 +0000

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_121641) do
+ActiveRecord::Schema.define(version: 2020_04_03_124553) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
